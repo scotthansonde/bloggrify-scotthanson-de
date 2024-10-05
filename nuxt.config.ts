9b0b0eea3bd9d45a1147f6a4a498382f52eb5240
Scott Hanson <papascott@gmail.com>
2024-10-05 15:03:29 +0200

    Update nuxt and nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,18 @@ export default defineNuxtConfig({
         '@bloggrify/core',
         '@bloggrify/mistral',
     ],
+    compatibilityDate: '2024-07-11',
+     vite: {
+        define: {
+            'process.env.DEBUG': false,
+        },
+        // Suppress warning about legacy JS API in Sass
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    api: 'modern-compiler',
+                },
+            },
+        },
+    },
 })
