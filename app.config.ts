d39d50bca644e2c502b7d234c171cd2dfeb049ad
Scott Hanson <papascott@gmail.com>
2024-10-06 13:19:22 +0200

    Initial app settings ScottHanson.DE

--- app.config.ts
+++ app.config.ts
@@ -1,21 +1,21 @@
 export default defineAppConfig({
-    logo: '/images/logo.svg',
+    logo: 'https://fav.farm/😎',
 
-    url: 'https://mistral.bloggrify.com/',
+    url: 'https://scotthanson.de/',
 
     theme: 'mistral',
 
     // The name of the blog itself
-    name: 'Bloggrify',
-    avatar: '/images/avatar.jpg',
+    name: 'ScottHanson.DE',
+    avatar: 'https://gravatar.com/avatar/910fa78695455feb1e28aecb4ce3debf?size=256&cache=1728119757242',
     // The description of the blog if any
-    description:  'lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.',
+    description:  'Saving it up for Friday night',
 
     analytics: {
-        provider: 'pirsch',
-        pirsch: {
-            code: 'UMgcWhIpdgfYXWLqLmCesiKf6vpSZBfv'
-        }
+        // provider: 'pirsch',
+        // pirsch: {
+        //     code: 'UMgcWhIpdgfYXWLqLmCesiKf6vpSZBfv'
+        // }
     },
 
     socials: {
@@ -33,16 +33,16 @@ export default defineAppConfig({
     },
 
     newsletter: {
-        enabled: true,
+        enabled: false,
         form_action: 'rssfeedpulse',
         provider: 'https://rssfeedpulse.com/api/campaign/996539cf-73e4-47b5-8d7c-2d7450174467/subscribe'
     },
 
     comments: {
-        enabled: true,
-        hyvor_talk: {
-            website_id: '10519', // Replace with your Hyvor Talk website ID (it won't work with this one)
-        },
+        enabled: false,
+        // hyvor_talk: {
+        //     website_id: '10519', // Replace with your Hyvor Talk website ID (it won't work with this one)
+        // },
     },
 
     table_of_contents: false,
@@ -52,28 +52,28 @@ export default defineAppConfig({
     // and the mistral theme use the default author for the main page
     authors: [
         {
-            username: 'hugo',
+            username: 'scotthansonde',
             default: true,
-            name: 'Hugo Lassiège',
+            name: 'Scott Hanson',
             description:
-                'Main maintainer of Bloggrify',
-            avatar: '/images/avatar.jpeg',
+                "I'm here and you're not",
+            avatar: 'https://gravatar.com/avatar/910fa78695455feb1e28aecb4ce3debf?size=256&cache=1728119757242',
             socials: {
-                twitter: 'https://twitter.com/hugolassiege',
-                twitter_username: 'hugolassiege',
-                mastodon: 'https://piaille.fr/@hugolassiege',
-                youtube: 'https://youtube.com/@eventuallycoding',
-                linkedin: 'https://www.linkedin.com/in/hugolassiege/',
+                twitter: 'https://twitter.com/scotthansonde',
+                twitter_username: 'scotthansonde',
+                mastodon: 'https://norden.social/@scotthansonde',
+                // youtube: 'https://youtube.com/@eventuallycoding',
+                // linkedin: 'https://www.linkedin.com/in/hugolassiege/',
                 // facebook: 'https://facebook.com',
                 // instagram: 'https://instagram.com',
-                github: 'https://github.com/hlassiege',
+                github: 'https://github.com/scotthansonde',
             },
         },
     ],
 
     menu: () => [
         { name: 'Home', path: '/' },
-        { name: 'Documentation', path: '/about' },
+        // { name: 'Documentation', path: '/about' },
         { name: 'Archives', path: '/archives' },
     ],
 
